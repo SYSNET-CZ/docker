5bd81c8374bdb22ea396f07ea98511cde7365578
Radim Jaeger <rjaeger@sysnet.cz>
2020-04-29 21:08:33 +0200

    d5

--- elk-dev/docker-entrypoint-es-plugins.sh
+++ elk-dev/docker-entrypoint-es-plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # setting up prerequisites
 elasticsearch-plugin install analysis-icu repository-s3
-exec /usr/local/bin/docker-entrypoint.sh.SAV elasticsearch
+exec /usr/local/bin/entrypoint.sh.SAV elasticsearch
 
